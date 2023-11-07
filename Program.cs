5bf398d7f0903af0d5d23b084662b506e8d090a6
LC1D_03_kuramochi <144000188+aiueoxyz@users.noreply.github.com>
2023-11-07 16:07:20 +0900

    リモートリポジトリでコンフリクト変更

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Git World!");
 Console.WriteLine("PullRequest Branch changed\n");
+std::cout << "Clone changed\n";
